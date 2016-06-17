db65310b554c74ae5018a22c5123c969fbac36f6
Nicholas Dille <nicholasdille@users.noreply.github.com>
2016-06-17 16:34:32 +0200

    Simplified the example

--- dsc/SetNodeName.ps1
+++ dsc/SetNodeName.ps1
@@ -23,13 +23,4 @@ Configuration SetNodeName {
 SetNodeName -ComputerName $ComputerName -OutputPath "$PSScriptRoot\Output"
 Set-DscLocalConfigurationManager -Path "$PSScriptRoot\Output" -Verbose
 
-if ($args.Count -gt 0) {
-    $Command = $args[0]
-    If ($args.Count -gt 1) {
-        $Arguments = $args[1..($args.Count - 1)]
-    }
-    & $Command $Arguments
-
-} else {
-    while ($true) { Start-Sleep -Seconds 60 }
-}
+ping -t localhost
