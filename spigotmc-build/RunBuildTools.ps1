18d37cd9d930966e9231cd9dbeb7a3e1f9dcaa7c
Nicholas Dille <nicholas.dille@mailbox.org>
2016-06-20 06:57:52 -0700

    Finalized build env for SpigotMC

--- spigotmc-build/RunBuildTools.ps1
+++ spigotmc-build/RunBuildTools.ps1
@@ -1,9 +1,5 @@
 $Env:JAVA_HOME = "$Env:ProgramFiles\Java\jre1.8.0u91"
 $Env:Path = "$Env:Path;$Env:ProgramFiles\Git\bin;$JAVA_HOME\bin"
 
-cd c:\build
-
-Invoke-WebRequest -Uri "$Env:BuildToolsUrl" -OutFile ".\BuildTools.jar"
-
-#& "bash.exe" --login -i -c "java.exe -jar "$PSScriptRoot\BuildTools.jar" --rev 1.9.2"
-java.exe -jar ".\BuildTools.jar"
+Set-Location -Path c:\build
+java.exe -jar BuildTools.jar
--- spigotmc-build/docker-build.cmd
+++ spigotmc-build/docker-build.cmd
@@ -1,3 +1,5 @@
 docker build -t nicholasdille/spigotmc-build .
-Rem docker run -d --name build nicholasdille/spigotmc-build
-Rem docker cp build:c:\build\spigot-*.jar .
+
+docker run -d --name build -w c:\build nicholasdille/spigotmc-build powershell -command .\RunBuildTools.ps1
+
+docker cp build:c:\build\spigot-*.jar .
