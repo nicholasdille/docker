3d6405a760f6bf5acfbc5d1c3f6c47c3039a263a
Nicholas Dille <nicholas.dille@mailbox.org>
2016-06-20 04:26:44 -0700

    Basing minecraft containers on dedicated javaruntime container

---
+++ spigotmc/minecraft.ps1
@@ -0,0 +1,5 @@
+#Set JAVA_HOME=%ProgramFiles%\Java\jre1.8.0_91
+#Set PATH=%PATH%;%JAVA_HOME%\Bin
+
+Set-Location -Path \minecraft
+& "$Env:ProgramFiles\Java\jre1.8.0_91\bin\Java.exe" -Xmx1024M -Xms32M -jar spigot-1.9.2.jar -W .\worlds
