ba77143f0e4f102e487b07d2571191627c7dbab9
ndille <ndille>
2016-06-23 16:52:00 +0200

    Repeated changes after conflict

--- spigotmc-build/docker-build.cmd
+++ spigotmc-build/docker-build.cmd
@@ -1,9 +1,3 @@
 @Echo Off
 
 docker build -t nicholasdille/spigotmc-build .
-
-docker run -d --name build -w c:\build nicholasdille/spigotmc-build powershell -command .\RunBuildTools.ps1
-docker wait build
-docker logs --tail=5 build
-
-for /f "usebackq tokens=4" %%i in (`docker logs --tail=1 build`) do docker cp build:c:\build\%%i .
