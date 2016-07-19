4ae4570113cd543b66b756f7a1b7193a7833203e
ndille <ndille>
2016-07-19 21:17:56 +0200

    Moved download code to separate script

---
+++ java/docker-build.ps1
@@ -0,0 +1,4 @@
+﻿"$PSScriptRoot\Download.ps1"
+
+& docker build -t nicholasdille/javaruntime:$JavaVersion "$PSScriptRoot"
+& docker tag nicholasdille/javaruntime:$JavaVersion nicholasdille/javaruntime:latest
