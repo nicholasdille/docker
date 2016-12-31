2a9a31f4f09b8c645d58b43fb231b2b2fbfe0a49
ndille <ndille>
2016-12-31 14:38:42 +0100

    Now builds with java as well

--- git/docker-build.ps1
+++ git/docker-build.ps1
@@ -1,8 +1,10 @@
-﻿"$PSScriptRoot\Download.ps1"
+﻿. "$PSScriptRoot\Download.ps1"
 
 'Building image'
-docker build -t nicholasdille/git:$GitVersion "$PSScriptRoot"
+docker build -f Dockerfile.core -t nicholasdille/git:$GitVersion "$PSScriptRoot"
+docker build -f Dockerfile.java-core -t nicholasdille/git:$($GitVersion)-java "$PSScriptRoot"
 
 If ($GitHubReleaseUri -like '*/latest') {
     docker tag nicholasdille/git:$GitVersion nicholasdille/git:latest
+    docker tag nicholasdille/git:$($GitVersion)-java nicholasdille/git:java
 }
