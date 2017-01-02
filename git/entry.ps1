c2671ccfa2618b490b5b89815b8965a6bdbff496
BuildTools <unconfigured@null.spigotmc.org>
2017-01-02 10:30:52 +0100

    Fixed parameter for git version

--- git/entry.ps1
+++ git/entry.ps1
@@ -2,7 +2,7 @@ $env:PATH += ";c:\git\bin"
 
 if ($args.Length -eq 0) {
     $RunCommand = 'git'
-    $RunParams = @('-version')
+    $RunParams = @('--version')
 } else {
     $RunCommand = $args[0]
     $RunParams = $args[1 .. ($args.Length - 1)]
