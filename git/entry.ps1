a7aff943e031bb05c5b6fedd423135c223b69aae
BuildTools <unconfigured@null.spigotmc.org>
2017-01-02 10:26:06 +0100

    Updates PATH in entry point

---
+++ git/entry.ps1
@@ -0,0 +1,11 @@
+$env:PATH += ";c:\git\bin"
+
+if ($args.Length -eq 0) {
+    $RunCommand = 'git'
+    $RunParams = @('-version')
+} else {
+    $RunCommand = $args[0]
+    $RunParams = $args[1 .. ($args.Length - 1)]
+}
+
+& $RunCommand @RunParams
