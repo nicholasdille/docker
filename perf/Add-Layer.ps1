39f4a20006ef1a0bfcf44777a1f79fc0b0db6a95
Nicholas Dille <nicholas.dille@mailbox.org>
2017-01-09 16:53:59 +0100

    Fix for multiplying a char

--- perf/Add-Layer.ps1
+++ perf/Add-Layer.ps1
@@ -7,7 +7,7 @@ New-Item -ItemType Directory -Path "$PSScriptRoot\Layer_$LayerIndex" | Out-Null
 1..$env:LAYER_FILE_COUNT | ForEach-Object {
     $FileIndex = $_
 
-    $RandomChar = [char](Get-Random -Minimum 0 -Maximum 255)
+    $RandomChar = [string][char](Get-Random -Minimum 0 -Maximum 255)
     $RandomSize = Get-Random -Minimum 1 -Maximum $env:LAYER_FILE_SIZE
     $RandomChar * $RandomSize | Set-Content -Path "$PSScriptRoot\Layer_$LayerIndex\File_$FileIndex"
 } | Out-Null
