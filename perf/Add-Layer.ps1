102b15d4433a750586b0ca90e70ac1af3b40fe56
Nicholas Dille <nicholas.dille@mailbox.org>
2017-01-09 16:51:13 +0100

    Randomize size and content

--- perf/Add-Layer.ps1
+++ perf/Add-Layer.ps1
@@ -7,15 +7,7 @@ New-Item -ItemType Directory -Path "$PSScriptRoot\Layer_$LayerIndex" | Out-Null
 1..$env:LAYER_FILE_COUNT | ForEach-Object {
     $FileIndex = $_
 
-    # SLOW
-    #$out = @()
-    #0..$env:LAYER_FILE_SIZE | ForEach-Object {
-    #    $out += Get-Random -Minimum 0 -Maximum 255
-    #}
-    #[System.IO.File]::WriteAllBytes("$PSScriptRoot\Layer_$LayerIndex\File_$FileIndex", $out)
-
-    # ZERO SIZE
-    #New-Item -ItemType File -Path "$PSScriptRoot\Layer_$LayerIndex\File_$FileIndex"
-
-    ' ' * $env:LAYER_FILE_SIZE | Set-Content -Path "$PSScriptRoot\Layer_$LayerIndex\File_$FileIndex"
+    $RandomChar = [char](Get-Random -Minimum 0 -Maximum 255)
+    $RandomSize = Get-Random -Minimum 1 -Maximum $env:LAYER_FILE_SIZE
+    $RandomChar * $RandomSize | Set-Content -Path "$PSScriptRoot\Layer_$LayerIndex\File_$FileIndex"
 } | Out-Null
