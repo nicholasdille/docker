985cec26600cb8386d137da55720516c3bdae5ad
Nicholas Dille <nicholas.dille@mailbox.org>
2017-03-31 10:02:41 +0200

    Added proxy for apt and gradle

--- proxy/proxy.sh
+++ proxy/proxy.sh
@@ -6,4 +6,22 @@ then
         export HTTPS_PROXY=$PROXY
         export http_proxy=$PROXY
         export https_proxy=$PROXY
+
+        cat /etc/*-release | eval
+
+        if [ "$ID_LIKE" == "debian" ]
+        then
+                cat > /etc/apt/apt.conf.d/01proxy < EOF
+Acquire::http::proxy "$PROXY";
+Acquire::https::proxy "$PROXY";
+EOF
+        fi
+
+        mkdir ~/.gradle
+        cat > ~/.gradle/gradle.properties < EOF
+systemProp.http.proxyHost=$PROXY_HOST
+systemProp.http.proxyPort=$PROXY_PORT
+systemProp.https.proxyHost=$PROXY_HOST
+systemProp.https.proxyPort=$PROXY_PORT
+EOF
 fi
