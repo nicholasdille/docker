6f92e89343cf902b7a77e6ffb11cec8db57b6ce0
Nicholas Dille <nicholas.dille@mailbox.org>
2017-04-04 15:52:03 +0200

    Deprecated

--- Add-ContainerImage.ps1
+++
@@ -1,4 +0,0 @@
-﻿Install-PackageProvider -Name ContainerImage -Force
-Install-ContainerImage -Name WindowsServerCore
-Restart-Service -Name docker
-docker tag windowsservercore:10.0.14300.1000 windowsservercore:latest
