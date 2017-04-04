6b5dfb2a0f533d4a6c4ffade720e99a70b99ea04
Nicholas Dille <nicholas.dille@mailbox.org>
2017-04-04 15:37:14 +0200

    Build system for Docker images

---
+++ build.ps1
@@ -0,0 +1,39 @@
+#$ErrorActionPreference = 'Stop'
+#Set-PSDebug -Trace 1
+
+#region Import registry definition
+Import-LocalizedData -BaseDirectory "$PSScriptRoot" -FileName registry.psd1 -BindingVariable Registry
+#endregion
+
+#region Enumerate directories with image definition
+Get-ChildItem -Path "$PSScriptRoot" -File -Recurse -Filter 'image.psd1' | ForEach-Object {
+    Write-Host "Processing $($_.DirectoryName)"
+
+    #region Import image definition
+    Import-LocalizedData -BaseDirectory $_.Directory -FileName image.psd1 -BindingVariable Image
+    #endregion
+
+    #region Build image
+    docker build -t $($Image.Name):$($Image.Version) $($_.DirectoryName)
+    #endregion
+
+    #region Build public image name
+    $Repository = $Registry.User
+    if ($Registry.Host) {
+        $Repository = "$($Registry.Host)/$Repository"
+    }
+    #endregion
+
+    #region Tag image with public name(s)
+    foreach ($Tag in $Image.Tags) {
+        docker tag $($Image.Name):$($Image.Version) $Repository/$($Image.Name):$Tag
+    }
+    #endregion
+
+    #region Push image with public name(s)
+    foreach ($Tag in $Image.Tags) {
+        "docker push $Repository/$($Image.Name):$Tag"
+    }
+    #endregion
+}
+#endregion
---
+++ ruby/image.psd1
@@ -0,0 +1,9 @@
+@{
+    Name    = 'ruby'
+    Version = 'latest'
+    Tags = @(
+        '2.3.1'
+        '2.3'
+        '2'
+    )
+}
